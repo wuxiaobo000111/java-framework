fa993fac2c72e6b78a405e35eb23cf8c1247cdd9
wuxiaobo <wuxiaobo@didachuxing.com>
2019-04-16 18:20:30 +0800

    spring-cloud:eureka-client

---
+++ spring-cloud-group/eureka-client/src/main/java/com/bobo/springcloud/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,18 @@
+package com.bobo.springcloud.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+/**
+ * eureka客户端
+ */
+@EnableDiscoveryClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
