f3b3d7cca212b9cb0372b2a90a8a41d9c33a50ac
wuxiaobo <wuxiaobo@didachuxing.com>
2019-05-18 12:04:28 +0800

    spring

--- spring-resource/src/main/java/com/bobo/spring/learn/base/Test.java
+++ spring-resource/src/main/java/com/bobo/spring/learn/base/Test.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.context.support.XmlWebApplicationContext;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
@@ -17,26 +19,56 @@ import java.util.List;
 
 public class Test {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
 
-        /**
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("mytest.xml");
+        MyTestBean myTestBean = applicationContext.getBean("myTestBean", MyTestBean.class);
+        System.out.println(myTestBean.getTestStr());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*    *//**
          * 	super();
          * 		ignoreDependencyInterface(BeanNameAware.class);
          * 		ignoreDependencyInterface(BeanFactoryAware.class);
          * 		ignoreDependencyInterface(BeanClassLoaderAware.class);
          *  XmlBeanFactory构造函数中移除了这三个接口
-         */
-      /*  XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("mytest.xml"));
+         *//*
+        XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("mytest.xml"));
         MyTestBean bean = factory.getBean(MyTestBean.class);
-        System.out.println(bean.getTestStr());
-        BeanDefinition myTestBean = factory.getBeanDefinition("myTestBean");
-        String testStr = (String) myTestBean.getAttribute("testStr");
-        System.out.println(testStr);
-*/
+        MyTestBean myTestBean = (MyTestBean) factory.getSingleton("myTestBean");
+        System.out.println(myTestBean.getTestStr());
+        Object singleton = factory.getSingleton("myTestBean", MyTestBean::new);*/
+       /* SimpleDateFormat info = (SimpleDateFormat) factory.getBean("dateFormat");
+        System.out.println(info.format(new Date()));*/
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:mytest.xml");
-        SimpleDateFormat info = (SimpleDateFormat) applicationContext.getBean("dateFormat");
-        System.out.println(info.format(new Date()));
+
+        /**
+         * 反射代码
+         */
+        /*Class<User> loadClass = (Class<User>) Thread.currentThread().getContextClassLoader().loadClass("contomTaglib.User");
+        User user = loadClass.newInstance();
+        Method setUserName = loadClass.getMethod("setUserName", String.class);
+        Method setEmail = loadClass.getMethod("setEmail", String.class);
+        setEmail.invoke(user,"2244027504@qq.com");
+        System.out.println(user.toString());*/
+
+
+
+      /*  BeanDefinition myTestBean = factory.getBeanDefinition("myTestBean");
+        String testStr = (String) myTestBean.getAttribute("testStr");
+        System.out.println(testStr);*/
     }
 
 }
