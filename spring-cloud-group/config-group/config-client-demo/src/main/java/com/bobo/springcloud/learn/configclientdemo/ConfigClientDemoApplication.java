0fc7542e3a4f01835a130946eab3a11ca620a23e
wuxiaobo <wuxiaobo@didachuxing.com>
2019-06-08 17:07:54 +0800

    Spring-cloud:config例子

---
+++ spring-cloud-group/config-group/config-client-demo/src/main/java/com/bobo/springcloud/learn/configclientdemo/ConfigClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bobo.springcloud.learn.configclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientDemoApplication.class, args);
+    }
+
+}
