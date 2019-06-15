cb1abbe8cef699c4be5718106c387715d7b75b40
wuxiaobo <wuxiaobo@didachuxing.com>
2019-06-15 14:41:50 +0800

    springcloud:gateway通过java代码的方式创建网关

---
+++ spring-cloud-group/gateway-group/gateway-demo-by-java/src/main/java/com/bobo/springcloud/learn/gatewaydemobyjava/GatewayDemoByJavaApplication.java
@@ -0,0 +1,33 @@
+package com.bobo.springcloud.learn.gatewaydemobyjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayDemoByJavaApplication {
+
+    /**
+     * 基本的转发
+     * 当访问http://localhost:8080/jd
+     * 转发到http://jd.com
+     * @param builder
+     * @return
+     */
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                //basic proxy
+                .route(r ->r.path("/jd")
+                        .uri("http://jd.com:80/").id("jd_route")
+                ).build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayDemoByJavaApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-group/gateway-group/gateway-demo-by-java/src/test/java/com/bobo/springcloud/learn/gatewaydemobyjava/GatewayDemoByJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bobo.springcloud.learn.gatewaydemobyjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GatewayDemoByJavaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
