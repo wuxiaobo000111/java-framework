a3360a9f24f1b89e55f47d269bd9a5affb1fe7fb
wuxiaobo <wuxiaobo@didachuxing.com>
2019-07-10 19:56:17 +0800

    Sentinel:流量控制demo

---
+++ cloud-alibaba-group/sentienl-group/sentinel-gateway-adapter-group/sentinel-gateway/src/main/java/com/bobo/cloud/alibaba/learn/sentinelgateway/SentinelGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bobo.cloud.alibaba.learn.sentinelgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelGatewayApplication.class, args);
+    }
+
+}
